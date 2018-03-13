301718f8a0644beae56a92f2f605d2e611a5f984
Thomas Laroche <tho.laroche@gmail.com>
2018-03-13 18:32:49 +0100

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, tools
+import os
+
+
+class RapidjsonConan(ConanFile):
+    name = "rapidjson"
+    version = "1.1.0"
+    license = "https://github.com/Tencent/rapidjson/blob/v%s/license.txt" % version
+    url = "https://github.com/Mikayex/conan-rapidjson"
+    description = "A fast JSON parser/generator for C++ with both SAX/DOM style API"
+    exports = ["LICENSE.md"]
+    source_subfolder = "source_subfolder"
+    no_copy_source = True
+
+    def source(self):
+        source_url = "https://github.com/Tencent/rapidjson"
+        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self.source_subfolder)
+
+    def package(self):
+        include_folder = os.path.join(self.source_subfolder, "include")
+        self.copy(pattern="license.txt", dst="licenses", src=self.source_subfolder)
+        self.copy(pattern="*", dst="include", src=include_folder)
+
+    def package_id(self):
+        self.info.header_only()
